8644466b619d9f3b8b5d4d23d9afb08a92841c25
Meltrox0 <dillonclynch@gmail.com>
2023-02-28 11:15:22 -0500

    Week 3

--- Assets/Code/PlayerControl.cs
+++ Assets/Code/PlayerControl.cs
@@ -1,28 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerControl : MonoBehaviour
 {
+    public float speed = 5f;
+
+    int health = 3;
+    public TMP_Text healthText;
+
+  //public float speed;
+
+  //public Vector3 start;
+  //public Vector3 end;
+  //Vector3 target;
+
     // Start is called before the first frame update
     void Start()
     {
+        healthText.text = health.ToString();
+
+      //target = end;
     }
 
     // Update is called once per frame
     void Update()
     {
-       // transform.position = new Vector3(2.5f, 1.6f, 2.5f); 
+       
        Vector3 newPos = transform.position;
+
        if(Input.GetKey(KeyCode.D)){
-       newPos.x+=0.1f;}
+       newPos.x = newPos.x + speed * Time.deltaTime;}
+
        if(Input.GetKey(KeyCode.A)){
-       newPos.x+=-0.1f;}
+       newPos.x = newPos.x - speed * Time.deltaTime;}
+
        if(Input.GetKey(KeyCode.W)){
-       newPos.z+=0.1f;}
+       newPos.z = newPos.z + speed * Time.deltaTime;}
+
        if(Input.GetKey(KeyCode.S)){
-       newPos.z+=-0.1f;}
+       newPos.z = newPos.z - speed * Time.deltaTime;}
        
        transform.position = newPos;
+       
+    }
+
+    //int AddSomeNumbers(int a, int b){
+    //    int sum = a+b;
+    //}
+
+    void OnCollisionEnter(Collision otherThing){
+        Debug.Log(otherThing.gameObject.name);
+        if(otherThing.gameObject.name == "NotABuddy"){
+            health --;
+            healthText.text = health.ToString();
+        }
+
+        if(otherThing.gameObject.name == "NotABuddy (1)"){
+            health --;
+            healthText.text = health.ToString();
+        }
+
+        if(otherThing.gameObject.name == "NotABuddy (2)"){
+            health --;
+            healthText.text = health.ToString();
+        }
     }
 }
